2c1302ce2ebd6dcaad35023436716d89b4c7e7c5
namrata <namrataap725@gmail.com>
2025-07-05 21:46:18 +0545

    about page added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,21 @@
+import style from "./style/main.module.css";
+
 function Main() {
   return (
-    <div>
-      <h1>Blog App</h1>
+    <div className={style.aboutContainer}>
+      <div className={style.aboutContent}>
+        <h2>About Our App</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+          delectus ducimus adipisci est, ab similique magni cum provident omnis,
+          quasi sit neque natus obcaecati incidunt dignissimos placeat corrupti
+          blanditiis consequuntur autem commodi aliquid quaerat! Delectus
+          repellendus quibusdam iusto excepturi voluptatem! Lorem ipsum, dolor
+          sit amet consectetur adipisicing elit. Placeat molestiae itaque veniam
+          nam accusamus quidem officiis quae quia asperiores blanditiis?
+        </p>
+      </div>
+      <div className={style.aboutBg}></div>
     </div>
   );
 }
