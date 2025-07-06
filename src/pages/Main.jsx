df03b4be9ff3613df9fc441369ecca8eb34f66f3
namrata <namrataap725@gmail.com>
2025-07-06 19:11:15 +0545

    make seperate component for about

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,25 +1,10 @@
+import About from "../component/About";
 import AboutMountains from "../component/AboutMountains";
-import style from "./style/main.module.css";
 
 function Main() {
   return (
     <>
-      <div className={style.aboutContainer}>
-        <div className={style.aboutContent}>
-          <h2>About Our App</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
-            delectus ducimus adipisci est, ab similique magni cum provident
-            omnis, quasi sit neque natus obcaecati incidunt dignissimos placeat
-            corrupti blanditiis consequuntur autem commodi aliquid quaerat!
-            Delectus repellendus quibusdam iusto excepturi voluptatem! Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Placeat
-            molestiae itaque veniam nam accusamus quidem officiis quae quia
-            asperiores blanditiis?
-          </p>
-        </div>
-        <div className={style.aboutBg}></div>
-      </div>
+      <About />
 
       <div>
         <AboutMountains />
