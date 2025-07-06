18b2f8d1e4507bfd77897b623de68fcc4f3e55b5
namrata <namrataap725@gmail.com>
2025-07-06 22:18:54 +0545

    import about image

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,26 +1,20 @@
-import { Link } from "react-router-dom";
+import aboutImg from "../img/about.jpg";
 import style from "./about.module.css";
+
 function About() {
   return (
     <div className={style.aboutContainer}>
       <div className={style.aboutContent}>
         <h2>About Our App</h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
-          delectus ducimus adipisci est, ab similique magni cum provident omnis,
-          quasi sit neque natus obcaecati incidunt dignissimos placeat corrupti
-          blanditiis consequuntur autem commodi aliquid quaerat! Delectus
-          repellendus quibusdam iusto excepturi voluptatem! Lorem ipsum, dolor
-          sit amet consectetur adipisicing elit. Placeat molestiae itaque veniam
-          nam accusamus quidem officiis quae quia asperiores blanditiis?
-        </p>
+        <p>{/* content */}</p>
         <Link to="/all" className={style.button}>
           <button> View Blogs</button>
         </Link>
       </div>
-      <div className={style.aboutBg}></div>
+      <div
+        className={style.aboutBg}
+        style={{ backgroundImage: `url(${aboutImg})` }}
+      />
     </div>
   );
 }
-
-export default About;
